ad31bda9e4a2d787f07e1d7dbd2ed80751c93cd5
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-14 14:25:22 +0100

    3 (group by) done

--- Queries.sql
+++ Queries.sql
@@ -56,17 +56,20 @@ WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
-SELECT COUNT(*) AS `inscritti`, YEAR(`enrolment_date`) AS `year` 
+SELECT COUNT(*) AS 'inscritti', YEAR(`enrolment_date`) AS `year` 
 FROM `students`
 GROUP BY(`year`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
-SELECT COUNT(*) AS  `teachers`, `office_address` AS  `address`
+SELECT COUNT(*) AS  'teachers' , `office_address` AS  `address`
 FROM `teachers`
 GROUP BY(`address`);
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
+SELECT `exam_id` AS 'exam', ROUND(AVG(`vote`)) AS 'avarege vote'
+FROM `exam_student`
+GROUP BY(`exam_id`);
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
