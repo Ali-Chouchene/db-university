c09a4354425befe9a760b20e801ab27b8f0aac32
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-14 14:36:22 +0100

    4 (group by) done

--- Queries.sql
+++ Queries.sql
@@ -73,3 +73,7 @@ FROM `exam_student`
 GROUP BY(`exam_id`);
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(`name`) AS 'corsi', `department_id` AS 'dipartimenti'
+FROM `degrees`
+GROUP BY(`department_id`);
