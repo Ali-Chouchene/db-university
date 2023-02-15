fa5deaae0d72c459787d8c963ab9c1277c58a6de
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-15 14:11:19 +0100

    1 (join) done

--- queries_1.sql
+++ queries_1.sql
@@ -1,6 +1,10 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
-
+SELECT `students`.`name` AS 'nome studente', `students`.`surname` AS 'cognome studente', `degrees`.`name` AS 'corso'
+FROM `degrees`
+JOIN `students`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
