f21b0a1ea2e03043e75e5ac1050263cfde5583e5
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-15 14:16:34 +0100

    2 (join) done

--- queries_1.sql
+++ queries_1.sql
@@ -8,7 +8,11 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
-
+SELECT `degrees`.`name` AS 'corso', `departments`.`name` AS 'dipartimento'
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
